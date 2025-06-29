00505af350fbd751d01409fc6060efb5d435f0d9
jonmaxmore <104246416+jonmaxmore@users.noreply.github.com>
2025-06-29 13:12:13 +0700

    Update api_service.dart

--- frontend/lib/core/network/api_service.dart
+++ frontend/lib/core/network/api_service.dart
@@ -1,3 +1,4 @@
+// lib/core/network/api_service.dart
 class ApiService {
   static Future<Map<String, dynamic>> authenticate(String username, String password) async {
     final response = await http.post(
