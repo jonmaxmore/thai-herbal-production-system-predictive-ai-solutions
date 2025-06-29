f21ea76c0ba1556be48477266c4241c074da19d5
jonmaxmore <104246416+jonmaxmore@users.noreply.github.com>
2025-06-29 13:04:04 +0700

    Create api_service.dart

---
+++ frontend/lib/core/network/api_service.dart
@@ -0,0 +1,10 @@
+class ApiService {
+  static Future<Map<String, dynamic>> authenticate(String username, String password) async {
+    final response = await http.post(
+      Uri.parse("${dotenv.env['API_BASE_URL']}/auth/login"),
+      body: jsonEncode({'username': username, 'password': password}),
+      headers: {'Content-Type': 'application/json'},
+    );
+    // ...
+  }
+}
